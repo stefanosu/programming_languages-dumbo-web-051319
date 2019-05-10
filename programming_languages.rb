8aaad30f6d9857e3e5f776da6db4b78c70931528
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 18:45:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
     language.each do |name, attributes|
     new_languages[name] ||= attributes
     new_languages[name][:style]=[]
-    new_languages[name][:style]<<style
+    new_languages[name][:style] << style
     end
   end
   new_languages
